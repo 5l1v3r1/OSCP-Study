3159a41b24a7813fccb88fb5f5ee95ec64f1e285
krubottom <karl.rubottom@gmail.com>
2017-06-03 21:40:22 -0600

    Update

--- recon/recon.py
+++ recon/recon.py
@@ -58,6 +58,8 @@ def main():
     parser.add_argument("--quiet",      dest="quiet",   action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.", default=False)
     parser.add_argument("--exec",       dest="follow",  action="store_true", help="Execute shell comamnds from recommendations as they are discovered. Likely to lead to very long execute times depending on the wordlist being used.", default=False)
     parser.add_argument("--simple_exec",dest="quickfollow",  action="store_true", help="Execute non-brute forcing shell comamnds only as they are discovered.", default=False)
+    parser.add_argument("--use-msf",    dest="use-msf", action="store_true", help="Check common Metasploit exploits.", default=False)
+    parser.add_argument("--pdf-report", dest="pdf-report", action="store_true" help="Generate host report in PDF format", default=False)
     arguments = parser.parse_args()
 
     if len(sys.argv) == 1:
